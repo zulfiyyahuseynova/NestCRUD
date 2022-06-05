8e61bd413b4eb30c0cc8637255cd12f059aca4eb
Zulfiyya Huseynova <tu7kswf4d@code.edu.az>
2022-06-05 22:00:10 +0400

    add

---
+++ NestTask/NestTask/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NestTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NestTask.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+
+    }
+}
